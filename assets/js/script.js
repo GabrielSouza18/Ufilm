f2e65c7ceeae0486c210617ca01896b8a0cf619f
Gabriel Souza <svgabriel18@hotmail.com>
2023-09-28 11:33:39 -0300

    add Responsively

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ let bannerMovie = document.querySelector('#banner-filme');
 let movieTitle = document.querySelector('#titulo-filme');
 let movieDescription = document.querySelector('#descricao');
 let movieButton = document.querySelector('#encontrar-filme');
+let movieButtonSinopse = document.querySelector('.ver-sinopse');
+let closeModalButton = document.querySelector('#closeModalButton');
+let modal = document.querySelector('#myModal');
 
 const API_KEY = 'e0d7645efbcad4ba43a36c7259e18592';
 const language = 'pt-BR';
@@ -11,6 +14,16 @@ movieButton.addEventListener('click', async () => {
     const randomIDMovie = randomNumber();
     const movie = await getMovie(randomIDMovie);
     renderMovie(movie);
+});
+
+// Abrir o modal quando o botão for clicado
+movieButtonSinopse.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Fechar o modal quando o botão de fechar for clicado
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
 });
 
 const randomNumber = () => Math.floor(Math.random() * 5000);
@@ -22,14 +35,13 @@ async function getMovie(id) {
 
 function renderMovie(movie) {
     try {
+        // Se você encontrou um filme, mostre o conteúdo do filme
         movieContainer.style.display = 'flex';
-        movieTitle.style.fontFamily = 'Poppins, sans-serif';
-        movieTitle.style.fontSize = '24px';
-        movieTitle.style.fontWeight = 'bold';
-        movieTitle.style.marginLeft = '25px';
-        bannerMovie.style.height = '400px';
 
-   
+
+
+        // Use JavaScript para controlar a visibilidade do botão ou outros elementos
+        movieButtonSinopse.style.display = 'block';
 
         bannerMovie.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
         movieTitle.textContent = movie.title;
@@ -38,6 +50,9 @@ function renderMovie(movie) {
         const errorMessage = "Erro 404 :( Tente novamente";
         movieTitle.textContent = errorMessage;
         bannerMovie.style.display = 'none';
-        movieDescription.textContent = ""; 
+        movieDescription.textContent = "";
+
+        // Use JavaScript para controlar a visibilidade do botão ou outros elementos
+        movieButtonSinopse.style.display = 'none';
     }
 }
