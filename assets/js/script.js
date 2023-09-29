dae835f6682cb313ba534d47b91fe22c979ba361
Gabriel Souza <svgabriel18@hotmail.com>
2023-09-29 02:54:05 -0300

    BUG FIXES

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,11 @@ let movieButton = document.querySelector('#encontrar-filme');
 let movieButtonSinopse = document.querySelector('.ver-sinopse');
 let closeModalButton = document.querySelector('#closeModalButton');
 let modal = document.querySelector('#myModal');
+let titleMovieModal = document.querySelector('.title-movie-modal');
+let descriptionMovieModal = document.querySelector('#description-movie-modal');
 
 const API_KEY = 'e0d7645efbcad4ba43a36c7259e18592';
 const language = 'pt-BR';
-
-movieButton.addEventListener('click', async () => {
-    const randomIDMovie = randomNumber();
-    const movie = await getMovie(randomIDMovie);
-    renderMovie(movie);
-});
-
-// Abrir o modal quando o botão for clicado
-movieButtonSinopse.addEventListener("click", () => {
-    modal.style.display = "block";
-});
-
-// Fechar o modal quando o botão de fechar for clicado
-closeModalButton.addEventListener("click", () => {
-    modal.style.display = "none";
-});
 
 const randomNumber = () => Math.floor(Math.random() * 5000);
 
@@ -32,27 +18,41 @@ async function getMovie(id) {
     const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=${language}`)
     return response.data;
 }
+movieButton.addEventListener('click', async () => {
+    const randomIDMovie = randomNumber();
+    try {
+        const movie = await getMovie(randomIDMovie);
+        renderMovie(movie);
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            const errorMessage = "Filme não encontrado :(";
+            movieTitle.textContent = errorMessage;
+            movieDescription.textContent = "";
+            movieButtonSinopse.style.display = 'none';
+        } else {
+            console.error(error);
+        }
+    }
+});
 
 function renderMovie(movie) {
-    try {
-        // Se você encontrou um filme, mostre o conteúdo do filme
-        movieContainer.style.display = 'flex';
-
-
-
-        // Use JavaScript para controlar a visibilidade do botão ou outros elementos
-        movieButtonSinopse.style.display = 'block';
-
-        bannerMovie.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        movieTitle.textContent = movie.title;
-        movieDescription.textContent = movie.overview;
-    } catch (error) {
-        const errorMessage = "Erro 404 :( Tente novamente";
-        movieTitle.textContent = errorMessage;
-        bannerMovie.style.display = 'none';
-        movieDescription.textContent = "";
-
-        // Use JavaScript para controlar a visibilidade do botão ou outros elementos
-        movieButtonSinopse.style.display = 'none';
-    }
+    movieContainer.style.display = 'flex';
+    movieButtonSinopse.style.display = 'block';
+    bannerMovie.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    // console.log(movie.poster_path)
+    movieTitle.textContent = movie.title;
+    movieDescription.textContent = movie.overview;
+    titleMovieModal.textContent = movie.title;
+    // console.log("Descrição do filme:", movie.overview);
+    descriptionMovieModal.textContent = movie.overview;
 }
+
+
+movieButtonSinopse.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
